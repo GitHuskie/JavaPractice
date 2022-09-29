e930c6ff09813c09d1cba4e180e2e5b4dc1f31cc
Yunmeng Zhang <244098057@qq.com>
2022-09-29 09:13:11 +0800

    commit

---
+++ src/ExArrAdd0929.java
@@ -0,0 +1,49 @@
+/**
+ * @author Yunmeng Zhang
+ * @version 2022.1
+ * @date 2022/9/29 8:39
+ */
+/*
+已知有一个升序数组,要求插入一个元素,该数组的顺序依然是升序,比如
+ */
+
+import java.util.Scanner;
+
+public class ExArrAdd0929 {
+    public static void main(String[] args) {
+        int[] arr = {10, 12, 45, 90};
+        int index = -1;
+
+        //数组扩容
+        int[] newArr = new int[arr.length + 1];
+
+        //比较大小
+        System.out.println("请输入您想要插入的元素...");
+        Scanner scanner = new Scanner(System.in);
+        int insertNum = scanner.nextInt();
+        for (int i = 0; i < arr.length; i++) {
+            if (insertNum <= arr[i]) {
+                index = i;
+                break;
+            }
+        }
+        //最大,放到最后
+        if (index == -1) {
+            index = arr.length;
+        }
+
+        //数组拷贝:如果是需要插入的位置,则j不自增,i自增
+        for (int i = 0, j = 0; i < newArr.length; i++) {
+            if (i == index) {
+                newArr[i] = insertNum;
+            } else {
+                newArr[i] = arr[j];
+                j++;
+            }
+        }
+        for (int i = 0; i < newArr.length; i++) {
+            System.out.print(newArr[i] + " ");
+        }
+        System.out.println("");
+    }
+}
